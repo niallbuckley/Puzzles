17ff302c6112210a12298bbba551602880d9c0a1
Niall Buckley <buckleyniall100@gmail.com>
2023-12-09 16:31:17 -0600

    in-memory storage rather than dictionary

--- aoc/day_9.py
+++ aoc/day_9.py
@@ -2,36 +2,6 @@
 f = open("test_data.txt").read().strip()
 seqs = [[int(x) for x in l.split()] for l in f.split('\n')]
 
-dic = {}
-
-def next_val(seq):
-    def recursion(i, line, new_line, layer):
-        if sum(line) == 0:
-            return layer
-        
-        dic[layer] = line[-1]
-        for i in range(len(line)-1):
-            new_line.append(line[i+1] - line[i])
-        
-        return recursion(0, new_line, [], layer + 1)
-
-        
-    
-    return recursion(0,seq, [], 0)
-
-ans = 0
-for seq in seqs:
-    layer = next_val(seq)
-    prev = dic[layer-1]
-    for i in range(layer-2,-1,-1):
-        prev = dic[i] + prev
-    ans += prev
-print ("ans:", ans)
-
-# Solution 2 is simply reversing the seq before recursive call.
-    
-## in-memory storage rather than dictionary
-
 def get_next_in_seq(sq):
     if not any(sq):
         return 0
@@ -46,3 +16,5 @@ result = 0
 for seq in seqs:
     result += get_next_in_seq(seq)
 print("ans " + str(result))
+
+# Solution 2 is simply reversing the seq before recursive call.
